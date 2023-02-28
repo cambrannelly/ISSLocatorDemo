e470cc6622f5a881fdeb3893d04932c0a77a8f4d
Cam Brannelly <cameron@krikey.com>
2023-02-28 11:28:49 -0700

    minor clean up to animations

--- ISS Locator/Views/SwiftUI/PulsingView.swift
+++ ISS Locator/Views/SwiftUI/PulsingView.swift
@@ -8,15 +8,21 @@
 import SwiftUI
 
 /// View with pulsing animation.
+///
+/// - Parameters:
+///   - diameter: A `CGFloat` that determines the diameter of the animation.
+///   - lineWidth: A `CGFloat` that determines the width of the animations.
 struct PulsingView: View {
+    var diameter: CGFloat = 80
+    var lineWidth: CGFloat = 2
     @State var animate = false
     
     var body: some View {
         ZStack {
-            Circle().stroke(Color.gray.opacity(animate ? 0 : 1)).frame(width: 60, height: 100).scaleEffect(animate ? 1 : 0)
-            Circle().stroke(Color.gray.opacity(animate ? 0 : 1)).frame(width: 40, height: 80).scaleEffect(animate ? 1 : 0)
-            Circle().stroke(Color.gray.opacity(animate ? 0 : 1)).frame(width: 20, height: 50).scaleEffect(animate ? 1 : 0)
-            Circle().stroke(Color.gray).frame(width: 0, height: 0)
+            Circle().stroke(Color(uiColor: UIColor.gray).opacity(animate ? 0 : 1), lineWidth: lineWidth).frame(width: diameter, height: diameter).scaleEffect(animate ? 1 : 0)
+            Circle().stroke(Color(uiColor: UIColor.gray).opacity(animate ? 0 : 1), lineWidth: lineWidth).frame(width: diameter * 0.75, height: diameter * 0.75).scaleEffect(animate ? 1 : 0)
+            Circle().stroke(Color(uiColor: UIColor.gray).opacity(animate ? 0 : 1), lineWidth: lineWidth).frame(width: diameter * 0.45, height: diameter * 0.45).scaleEffect(animate ? 1 : 0)
+            Circle().stroke(Color(uiColor: UIColor.gray), lineWidth: lineWidth).frame(width: 0, height: 0)
         }
         .onAppear {
             withAnimation(Animation.easeInOut(duration: 1.5).repeatForever(autoreverses: false)) {
